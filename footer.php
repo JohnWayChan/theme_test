9d15f5300dab8fee9b1aeeaf51ae04267e95c4f4
John <863164757@qq.com>
2016-04-19 02:54:09 +0000

    first commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="spacer"></div>
+
+<div id="footer">
+    Copyright © 2011 Theme Test by <a href="http://www.johnwaychan.com">JohnChan</a><br />
+</div>
+
+</div><!-- content -->
+<?php get_sidebar(); ?>
---
+++ sidebar.php
@@ -0,0 +1,19 @@
+<div id="sidebar">
+    <div class="sidebar-box">
+        <span class="sidebar-title">关于我</span>
+        <div class="dots"></div>
+        <div class="textwidget">
+            <p>程序设计师 & 剑士</p>
+        </div>
+    </div>
+    <div class="sidebar-box">
+        <span class="sidebar-title">recent posts</span><div class="dots"></div>
+        <ul>
+            <li><a href="" title="Hello world!">Hello world!</a></li>
+        </ul>
+    </div>
+</div><!-- sidebar -->
+<div class="spacer"></div>
+</div><!-- wrapper -->
+</body>
+</html>
